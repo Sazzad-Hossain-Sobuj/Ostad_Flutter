5964560fdec51d7695bf0af5daf170a476f9a13d
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-09-15 10:07:29 +0600

    1-grid view and it's property

---
+++ lib/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Grid-View',style: TextStyle(fontWeight: FontWeight.bold),),
+        centerTitle: true,
+        toolbarHeight: 100,
+        elevation: 30,
+        backgroundColor: Colors.teal,
+        
+        leading: Icon(Icons.home),
+        actions: [
+          IconButton(onPressed: (){},
+              icon: Icon(Icons.menu)),
+        ],
+      ),
+
+      body: Center(
+        child: GridView(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10),
+        children: [
+          Image.network('https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg'),
+          Image.network('https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg'),
+          Image.network('https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg'),
+          Image.network('https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg'),
+          Image.network('https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg'),
+          Image.network('https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg'),
+          Image.network('https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg'),
+          Image.network('https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg'),
+        ],
+        ),
+
+      ),
+
+    );
+  }
+}
