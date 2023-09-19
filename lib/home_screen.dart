4b060d47a0428007b1b6ac42be149c0d4efceadd
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-09-19 12:21:02 +0600

    module6 tabBar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,48 +1,34 @@
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-   HomeScreen({Key? key}) : super(key: key);
-
-  var MyItems = [
-    {'img':'https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg','title':'cartoon 1'},
-    {'img':'https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg','title':'cartoon 2'},
-    {'img':'https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg','title':'cartoon 3'},
-    {'img':'https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg','title':'cartoon 4'},
-    {'img':'https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg','title':'cartoon 5'},
-    {'img':'https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg','title':'cartoon 6'},
-  ];
-  
-  MySnackBar(context,msg){
-    return ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(msg)));
-  }
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dynamic ListView builder'),
-      ),
-      body: ListView.builder(
-        shrinkWrap: true,
-        itemCount: MyItems.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              MySnackBar(context, MyItems[index]['title']);
-            },
-            child: Container(
-              margin: EdgeInsets.all(10),
-              height: 200,
-              width: double.infinity,
-              child: Image.network(
-                MyItems[index]['img']!,
-                fit: BoxFit.cover,
-              ),
+    return DefaultTabController(
+        length: 8,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.teal,
+            title: Text('TabBar'),
+            bottom: TabBar(
+              isScrollable: true,
+                tabs: [
+                  Tab(icon: Icon(Icons.home), text: 'Home',),
+                  Tab(icon: Icon(Icons.no_accounts), text: 'Accounts'),
+                  Tab(icon: Icon(Icons.add_business), text: 'Business',),
+                  Tab(icon: Icon(Icons.bubble_chart), text: 'Bubble',),
+                  Tab(icon: Icon(Icons.shopping_cart), text: 'Shopping chart',),
+                  Tab(icon: Icon(Icons.shopping_basket), text: 'Shoppin busket',),
+                  Tab(icon: Icon(Icons.notifications), text: 'Notification',),
+                  Tab(icon: Icon(Icons.menu), text: 'Menu'),
+
+                ]
             ),
-          );
-        },
-      ),
+          ),
+        ),
     );
   }
 }
