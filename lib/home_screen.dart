219fcf70005824a3a3fdd2399535228b44fa9c29
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-10-05 21:22:33 +0600

    module 9 sum app

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,166 +1,117 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+/// TODO : Add multiply(*), division(/) and modulus(%)
 
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _fieldOneTEController = TextEditingController();
+  final TextEditingController _fieldTwoTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  double result = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text('Sum Calculator'),
       ),
-
-      body: OrientationBuilder(
-        builder: (context, orientation){
-          if(orientation == Orientation.portrait){
-            return PortraitLayout();
-          }
-          else{
-            return LandscapeLayout();
-          }
-        },
-      ),
-
-    );
-  }
-}
-
-class PortraitLayout extends StatelessWidget {
-  PortraitLayout({Key? key}) : super(key: key);
-  final List<String> Images = [
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    // Add more image URLs here
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Stack(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              color: Colors.black,
-            ),
-            Positioned(
-                top: 10,
-                left: 17,
-                child: CircleAvatar(
-                  radius: 180,
-                  backgroundImage: NetworkImage('https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4'),
-                )),
-            Positioned(
-                top: 400,
-                left: 80,
-                child: Text('Sazzad Hossain',style: TextStyle(color: Colors.green,fontSize: 30),)),
-            Positioned(
-                top: 450,
-                child: Wrap(
-                  children: [
-                    Text("Hey! I'm Sazzad Hossain. Flutter Developer is available here.",style: TextStyle(color: Colors.white),)
-
-                  ],
-                )),
-            Positioned(
-                top: 480,
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                  ),
-                  itemCount: Images.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: Container(
-                        child: Image.network(
-                          Images[index],
-                          width: 150,
-                          height: 150,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  },
-                )
-                )
-        ],
-        ),
-    );
-  }
-}
-
-class LandscapeLayout extends StatelessWidget {
-  LandscapeLayout({Key? key}) : super(key: key);
-  final List<String> Images = [
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    "https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4",
-    // Add more image URLs here
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.black,
-          ),
-          Positioned(
-              top: 10,
-              left: 5,
-              child: CircleAvatar(
-                radius: 150,
-                backgroundImage: NetworkImage('https://scontent.fdac68-2.fna.fbcdn.net/v/t39.30808-1/364765015_1334384337489300_9211528646095833305_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-7&_nc_sid=fe8171&_nc_eui2=AeG8ek8gEpQja3Td34lDbWpH-913JNiG0a_73Xck2IbRr5glcTth6uIMLjCvhoZA324s-9aCuDWV24Jk8-a_JsFY&_nc_ohc=pRAHWLVvMpwAX_YK9xF&_nc_ht=scontent.fdac68-2.fna&oh=00_AfA1PcSaP-2IEoiZXfDKpnMoMAA1sGrXBYCTzV6JxsYDOQ&oe=651B38A4'),
-              )),
-          Positioned(
-              top: 10,
-              left: 330,
-              child: Text('Sazzad Hossain',style: TextStyle(color: Colors.green,fontSize: 30),)),
-          Positioned(
-              top: 50,
-              left: 330,
-              child: Wrap(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _fieldOneTEController,
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                    hintText: 'Field 1'
+                ),
+                validator: (String? value) {
+                  if (value == null) {
+                    return 'Enter a value';
+                  }
+                  if (value.trim().isEmpty) {
+                    return 'Enter a number';
+                  }
+                  // if (value?.trim().isEmpty ?? true) {
+                  //   return 'Enter valid value';
+                  // }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _fieldTwoTEController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    hintText: 'Field 2'
+                ),
+                validator: (String? value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Enter valid value';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16,),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
                 children: [
-                  Text("Hey! I'm Sazzad Hossain. Flutter Developer is available here.",style: TextStyle(color: Colors.white),)
-
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        double firstNumber = parseToDouble(_fieldOneTEController.text.trim());
+                        double secondNumber = parseToDouble(_fieldTwoTEController.text.trim());
+                        result = addition(firstNumber, secondNumber);
+                        setState(() {});
+                      }
+                    },
+                    icon: const Icon(Icons.add),
+                    label: const Text('Add'),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        double firstNumber = parseToDouble(_fieldOneTEController.text.trim());
+                        double secondNumber = parseToDouble(_fieldTwoTEController.text.trim());
+                        result = sub(firstNumber, secondNumber);
+                        setState(() {});
+                      }
+                    },
+                    icon: const Icon(Icons.remove),
+                    label: const Text('Sub'),
+                  ),
                 ],
-              )),
-          Positioned(
-            top: 80,
-            left: 330,
-            child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
-                itemCount: Images.length,
-                itemBuilder: (context,index){
-                  return Card(
-                    child: Container(
-                      child: Image.network(
-                        Images[index],
-                        fit: BoxFit.cover,
-                      ),
-
-                    ),
-                  );
-                }),
-
-           )
-
-
-        ],
+              ),
+              Text(
+                'Result is : $result',
+                style: const TextStyle(fontSize: 18),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  double parseToDouble(String text) {
+    return double.tryParse(text) ?? 0;
+  }
+
+  double addition(double firstNum, double secondNum,) {
+    return firstNum + secondNum;
+  }
+
+  double sub(double firstNum, double secondNum,) {
+    return firstNum - secondNum;
+  }
+
 }
